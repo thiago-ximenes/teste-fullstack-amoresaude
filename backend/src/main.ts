a6ede0b8e573d49f7fbbcf49f112abfe7925f468
ximenes <thiago@ximenes.dev>
2024-02-16 09:51:13 -0300

    feat(request): install and configure request validators

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {HttpStatus, ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    whitelist: true,
+  }))
   await app.listen(3000);
 }
 bootstrap();
