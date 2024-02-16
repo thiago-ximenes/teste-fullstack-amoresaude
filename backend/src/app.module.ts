cafbb003ea2f454f0324564259479aabda030149
ximenes <thiago@ximenes.dev>
2024-02-16 09:43:24 -0300

    feat(database): install typeorm and configure connection to mysql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import databaseConfig from "./config/database.config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [databaseConfig]
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (_configService: ConfigService) => ({
+                type: 'mysql',
+                entities: [
+                    __dirname + '/../**/*.entity{.ts,.js}',
+                ],
+                host: _configService.get('database.host'),
+                port: _configService.get('database.port'),
+                username: _configService.get('database.username'),
+                password: _configService.get('database.password')!,
+                synchronize: true,
+                autoLoadEntities: true,
+                logging: true,
+            }),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
